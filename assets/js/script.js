dfb6655f530380acdd059d1c07aa14f2a0f089b6
Marcos Camargo <marcoscamargo@outlook.com>
2022-04-16 23:34:48 -0300

    Slide function and fetch data from unsplash Hamburguer menu and toggle side test A few screen break points for make it responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,12 @@
-const element = document.getElementById("dark-mode-toggle");
+const darkToggle = document.getElementById("dark-mode-toggle");
 const toggleIcon = document.getElementById("toggle-icon");
+const slideBtn = document.getElementsByClassName("slide-btn");
+const imgSlide = document.getElementsByClassName("img-slide")[0];
+const sideBar = document.getElementsByClassName("sidebar")[0];
+const sideContent = document.getElementsByClassName("side-content")[0];
 
 const toggleDarkMode = () => {
-    element.classList.toggle("dark-mode");
+    darkToggle.classList.toggle("dark-mode");
     if(toggleIcon.classList.contains("fa-moon")) {
         toggleIcon.classList.remove("fa-moon");
         toggleIcon.classList.toggle("fa-sun");
@@ -11,4 +15,32 @@ const toggleDarkMode = () => {
         toggleIcon.classList.toggle("fa-moon");
     }
 
+}   
+
+const changeSlide = (num) => {
+    let btns = [...slideBtn];    
+    btns.forEach(btn => {
+        if(btn.classList.contains("slide-active")) {
+            btn.classList.remove("slide-active");
+        }
+    });
+    slideBtn[num].classList.add("slide-active");      
+    fetchSlide();
+}
+const fetchSlide = async () => {
+    response = await fetch("https://source.unsplash.com/random/?nature");
+    imgSlide.src = response.url;
+}
+
+const toggleSideBar = () => {
+    if(sideBar.style.display === "none"){   
+        sideBar.style.display = "flex";
+        sideContent.style.width = "85%";
+        sideContent.style.left = "15%";
+    }
+    else {
+        sideBar.style.display = "none";
+        sideContent.style.width = "100%";
+        sideContent.style.left = "0";
+    }   
 }
